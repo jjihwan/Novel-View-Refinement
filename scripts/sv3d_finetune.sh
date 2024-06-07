f96dab865823c21f7c438b010cd9066fce9f79c4
jjihwan <kjh26720@snu.ac.kr>
2024-06-07 12:34:59 +0900

    training on refined data

--- scripts/sv3d_finetune.sh
+++ scripts/sv3d_finetune.sh
@@ -1,5 +1,6 @@
 python3 scripts/training/train_sv3d.py \
-                        --name NVR-onepoint \
+                        --name NVR-refined-data \
                         --base configs/training/sv3d_p.yaml \
                         --projectname SV3D-fine-tune \
-                        --wandb
+                        --wandb \
+                        --no-test
