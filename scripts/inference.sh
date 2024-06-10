42a055661d7a115d4bc07e66a08aa2bafbea67b0
jjihwan <kjh26720@snu.ac.kr>
2024-06-10 15:12:12 +0900

    infer scripts

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -1,4 +1,4 @@
 python3 scripts/sampling/simple_video_sample.py \
                             --version sv3d_p \
-                            --input_path assets/dataset \
+                            --input_path assets/dataset_both \
                             --verbose True
